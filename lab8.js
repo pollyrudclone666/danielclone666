b0763a225cacddccd76003ded79020d2547ab5a9
pollyrudclone666 <fedenevdaniel@gmail.com>
2022-12-24 16:49:58 +0700

    Количктсво дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -3,4 +3,15 @@ function showdate(params) {
     let today = new Date();
     out.innerHTML = today.toLocaleDateString("ru-RU");
 }
+
+function showDaysCount(params) {
+    let today= new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.floor(daysCount); // Округляем в меньшую сторону
+    result.innerHTML = 'С даты рождения прошло столько дней: ' + daysCount;
+}
+
 window.addEventListener('load', showdate);
