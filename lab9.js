e2b0d556cd2466f24e5476b63b8848753a243d09
pollyrudclone666 <fedenevdaniel@gmail.com>
2022-12-27 17:47:04 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -5,3 +5,19 @@ function clickMe(button) {
         button.innerHTML = 'Щелчок';
 
 }
+
+function surname1(button) {
+    if(button.innerHTML == 'Феденев')
+        button.innerHTML = 'Даниэль';
+    else
+        button.innerHTML = 'Феденев';
+
+}
+
+function surname2(button) {
+    if(button.innerHTML == 'Руднева')
+        button.innerHTML = 'Аполлинария';
+    else
+        button.innerHTML = 'Руднева';
+
+}
